d8189179cface01818b1248fad31af9c88b10abb
Angus <ajdfajdfl2003@gmail.com>
2017-05-13 16:47:09 +0800

    Finish Two Sum
    What: Two Sum Tests
    What: Add TestTwoSum_nums_is_1_8_target_is_9_return_0_1 Test case
    What: [Green] TestTwoSum_nums_is_1_8_target_is_9_return_0_1
    What: Add What: TestTwoSum_nums_is_1_2_8_target_is_9_return_0_2 Test case
    [Green] TestTwoSum_nums_is_1_2_8_target_is_9_return_0_2
    What: Refactor
      - Why:
        - More effective
    What: Add TestTwoSum_nums_is_3_2_4_target_is_6_return_1_2 Test case
    [Green] TestTwoSum_nums_is_3_2_4_target_is_6_return_1_2

---
+++ 1TwoSum/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _1TwoSum
+{
+    internal class Solution
+    {
+        public Solution()
+        {
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var sorted = nums.Select((x, index) => Tuple.Create(x, index)).OrderBy(x => x.Item1).ToArray();
+            for (var i = 0; i < sorted.Length; i++)
+            {
+                for (var j = i + 1; j < sorted.Length; j++)
+                {
+                    var twoSums = sorted[i].Item1 + sorted[j].Item1;
+                    if (twoSums == target)
+                    {
+                        return new int[]
+                            {Math.Min(sorted[i].Item2, sorted[j].Item2), Math.Max(sorted[i].Item2, sorted[j].Item2)};
+                    }
+                    if (twoSums > target)
+                    {
+                        break;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ 1TwoSum/TwoSumTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _1TwoSum
+{
+    [TestClass]
+    public class TwoSumTests
+    {
+        [TestMethod]
+        public void TestTwoSum_nums_is_3_2_4_target_is_6_return_1_2()
+        {
+            var sut = new Solution();
+            var nums = new int[] {3, 2, 4};
+            var target = 6;
+
+            var actual = sut.TwoSum(nums, target);
+
+            var expected = new int[] {1, 2};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTwoSum_nums_is_1_2_8_target_is_9_return_0_2()
+        {
+            var sut = new Solution();
+            var nums = new int[] {1, 2, 8};
+            var target = 9;
+
+            var actual = sut.TwoSum(nums, target);
+
+            var expected = new int[] {0, 2};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTwoSum_nums_is_1_8_target_is_9_return_0_1()
+        {
+            var sut = new Solution();
+            var nums = new int[] {1, 8};
+            var target = 9;
+
+            int[] actual = sut.TwoSum(nums, target);
+
+            var expected = new int[] {0, 1};
+            CollectionAssert.AreEqual(actual, expected);
+        }
+    }
+}
