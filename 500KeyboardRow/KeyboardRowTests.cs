b9b21326cfdd33f4b96ee73a36bd8aa6c40f4066
Angus <ajdfajdfl2003@gmail.com>
2017-05-14 11:32:34 +0800

    Finish Keyboard Row
    What: Keyboard Row Tests
      - Why:
        - Init
    
    What: Add FindWordsTest_Input_asd_return_asd Test case
    
    What: [Green] FindWordsTest_Input_asd_return_asd
    
      - How:
        - Hard code
    
    What: Add FindWordsTest_Input_asdf_qaz_return_asd Test case
    
    What: [Green] FindWordsTest_Input_asdf_qaz_return_asd
    
    What: Add FindWordsTest_Input_Hello_Dad_return_Dad Test case
    
    What: [Green] FindWordsTest_Input_Hello_Dad_return_Dad
    
    What: Refactor
    
    What: Refactor to 59.72%
    
    What: Refactor to 82.46%
    
      - How:
        - If not in keyboard row in first place, return false.

---
+++ 500KeyboardRow/KeyboardRowTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _500KeyboardRow
+{
+    [TestClass]
+    public class KeyboardRowTests
+    {
+        [TestMethod]
+        public void FindWordsTest_Input_Hello_Dad_return_Dad()
+        {
+            var sut = new Solution();
+            var words = new string[] {"Hello", "Dad"};
+
+            var actual = sut.FindWords(words);
+
+            var expected = new string[] {"Dad"};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindWordsTest_Input_asdf_qaz_return_asd()
+        {
+            var sut = new Solution();
+            var words = new string[] {"asdf", "qaz "};
+
+            var actual = sut.FindWords(words);
+
+            var expected = new string[] {"asdf"};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindWordsTest_Input_asd_return_asd()
+        {
+            var sut = new Solution();
+            var words = new string[] {"asd"};
+
+            var actual = sut.FindWords(words);
+
+            var expected = new string[] {"asd"};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ 500KeyboardRow/Solution.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _500KeyboardRow
+{
+    public class Solution
+    {
+        public Solution()
+        {
+        }
+
+        public string[] FindWords(string[] words)
+        {
+            var firstRow = new Dictionary<string, string>
+            {
+                {"q", "q"},
+                {"w", "w"},
+                {"e", "e"},
+                {"r", "r"},
+                {"t", "t"},
+                {"y", "y"},
+                {"u", "u"},
+                {"i", "i"},
+                {"o", "o"},
+                {"p", "p"}
+            };
+            var secondRow = new Dictionary<string, string>
+            {
+                {"a", "a"},
+                {"s", "s"},
+                {"d", "d"},
+                {"f", "f"},
+                {"g", "g"},
+                {"h", "h"},
+                {"j", "j"},
+                {"k", "k"},
+                {"l", "l"}
+            };
+            var thirdRow = new Dictionary<string, string>
+            {
+                {"z", "z"},
+                {"x", "x"},
+                {"c", "c"},
+                {"v", "v"},
+                {"b", "b"},
+                {"n", "n"},
+                {"m", "m"}
+            };
+            var result = new List<string>();
+
+            foreach (var word in words)
+            {
+                if (CheckWord(word.ToLower(), firstRow)
+                    || CheckWord(word.ToLower(), secondRow)
+                    || CheckWord(word.ToLower(), thirdRow))
+                {
+                    result.Add(word);
+                }
+            }
+            return result.ToArray();
+        }
+
+        private bool CheckWord(string word, Dictionary<string, string> keyBoard)
+        {
+            return word.All(a => keyBoard.ContainsKey(a.ToString()));
+        }
+    }
+}
