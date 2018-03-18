2f63229954740a95983cd4abb39ca24a5e02ef41
AngusWu <anguswu@simbo.com.tw>
2018-03-18 21:50:34 +0800

    Finish test case Given_z_ZZ_Should_Get0

--- Java/LeetCodePractice/src/test/java/com/angus/leetcode/JewelsAndStones/SolutionTest.java
+++ Java/LeetCodePractice/src/test/java/com/angus/leetcode/JewelsAndStones/SolutionTest.java
@@ -14,6 +14,16 @@ public class SolutionTest {
     }
 
     @Test
+    public void Given_z_ZZ_Should_Get0() {
+        String J = "z";
+        String S = "ZZ";
+
+        int actual = this.solution.numJewelsInStones(J, S);
+
+        assertThat(actual).isEqualTo(0);
+    }
+
+    @Test
     public void Given_aA_aAAbbbb_Should_Get3() {
         String J = "aA";
         String S = "aAAbbbb";
