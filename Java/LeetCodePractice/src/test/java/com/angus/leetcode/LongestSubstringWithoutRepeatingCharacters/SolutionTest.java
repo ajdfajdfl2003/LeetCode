b5fe8936d1ecc0c82b9e64aa6844c3280e4c117a
AngusWu <anguswu@simbo.com.tw>
2018-03-18 21:00:28 +0800

    Finish test case Given_bbbbb_Length_Should_Get1

--- Java/LeetCodePractice/src/test/java/com/angus/leetcode/LongestSubstringWithoutRepeatingCharacters/SolutionTest.java
+++ Java/LeetCodePractice/src/test/java/com/angus/leetcode/LongestSubstringWithoutRepeatingCharacters/SolutionTest.java
@@ -6,6 +6,16 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class SolutionTest {
     @Test
+    public void Given_bbbbb_Length_Should_Get1() {
+        String s = "bbbbb";
+        Solution target = Solution.newInstance();
+
+        int actual = target.lengthOfLongestSubstring(s);
+
+        assertThat(actual).isEqualTo(1);
+    }
+
+    @Test
     public void Given_abcabcbb_Length_Should_Get3() {
         String s = "abcabcbb";
         Solution target = Solution.newInstance();
